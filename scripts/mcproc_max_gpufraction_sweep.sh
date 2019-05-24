87009ab4c4e1e996e4129732161c7513bc3ca8c9
Sandeep D'souza <sandeepdsouza93@gmail.com>
2019-05-24 15:53:13 -0400

    adding final multiproc scripts

--- scripts/mcproc_max_gpufraction_sweep.sh
+++ scripts/mcproc_max_gpufraction_sweep.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# Sweep over GPU utilization
+# Sweep over the task with the largest fractional requirement of the GPU
 NUM_TASKSETS=5000
 HARMONIC_FLAG=0
-FILENAME=$1	#mcproc_gpu_util_sweep_.csv
+FILENAME=$1	#mcproc_gpu_fraction_sweep.csv
 EPSILON=0.01
 CPU_UTIL=1.5
+GPU_UTIL=0.4
 GPU_TASK_FRACTION=0.5
 MAX_GPU_SEGMENTS=10
 MAX_NUM_TASKS=10
-MAX_GPU_FRACTION=$2 #0.3 0.5 0.7 1.0
-MODE=0
+MODE=3
 NUM_CORES=4
-for gpu_util in {10..70..10}
+for max_gpu_fraction in {10..100..10}
 do
-	GPU_UTIL=$(echo "scale = 1; $gpu_util/100" | bc)
-	echo $GPU_UTIL
+	MAX_GPU_FRACTION=$(echo "scale = 1; $max_gpu_fraction/100" | bc)
+	echo $MAX_GPU_FRACTION
 	../src/mcprocessor_exp.out $NUM_TASKSETS $HARMONIC_FLAG $FILENAME $EPSILON $CPU_UTIL $GPU_UTIL $GPU_TASK_FRACTION $MAX_GPU_SEGMENTS $MAX_NUM_TASKS $MAX_GPU_FRACTION $MODE $NUM_CORES
 done
 exit 0
--- scripts/mcproc_utilsweep.sh
+++ scripts/mcproc_utilsweep.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+# Sweep over CPU utilization
 NUM_TASKSETS=5000
 HARMONIC_FLAG=0
-FILENAME=$1
+FILENAME=$1 #mcproc_cpu_util_sweep_.csv
 EPSILON=0.01
 GPU_UTIL=0.3
 GPU_TASK_FRACTION=0.5
 MAX_GPU_SEGMENTS=10
-MAX_NUM_TASKS=10
-MAX_GPU_FRACTION=0.3 #1.0
+MAX_NUM_TASKS=15
+MAX_GPU_FRACTION=$2 #0.3 0.5 0.7 1.0
 MODE=0
-NUM_CORES=4
-for cpu_util in {40..300..10}
+NUM_CORES=6 #4
+#for cpu_util in {40..300..10} # for 4 cores
+#for cpu_util in {300..600..20} # for 8 cores
+for cpu_util in {200..500..20} # for 6 cores
 do
 	CPU_UTIL=$(echo "scale = 1; $cpu_util/100" | bc)
 	echo $CPU_UTIL
